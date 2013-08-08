5fa5ec3cca439fcdcc3adea06087e61faf6d16f0
james2doyle <james2doyle@gmail.com>
2013-08-08 15:00:35 -0400

    add more comments. remove inefficient code.

--- js/socialist.js
+++ js/socialist.js
@@ -1,10 +1,12 @@
 function getInput(){
+  // hide the last item
+  $('.socialist_button').last().hide();
   $.post(BASE_URL + "streams_core/public_ajax/field/socialist/get_field", {}, function(res) {
     // inject the new component
     $("#parameters").append(res);
   }).then(function() {
-    // add the click event to the last item
-    $('.socialist_button').last().click(socialistClick);
+    // add the click event to the new last item
+    $('.socialist_button').last().show().click(socialistClick);
   });
 }
 
@@ -16,5 +18,7 @@ function socialistClick(e) {
 }
 
 $(document).ready(function(){
-  $('.socialist_button').unbind('click').click(socialistClick);
+  // hide all buttons but the last one
+  $('.socialist_button').hide();
+  $('.socialist_button').last().show().unbind('click').click(socialistClick);
 });
